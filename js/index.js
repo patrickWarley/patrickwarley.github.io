a94477fff3f8ef1350b9bc2940a44e8ad4f60879
Programadores Cariocas <programadores.cariocas@senacrjedu.local>
2022-09-13 20:20:08 -0300

    security test

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-function scrolledIntoView(){
-
-
-    var docViewTop = window.scrollY;
-    console.log(docViewTop);
-
-    var test = document.getElementById('hobbiesinterests');
-
-    console.log(test.offsetHeight);
-}
-
 //just use the intersect observer api
 https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
 
