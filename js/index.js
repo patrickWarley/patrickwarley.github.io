0ac9b207d628aec23458feab245aa29322f7a6c2
patrickwarley <patrickwarley@gmail.com>
2023-03-26 17:02:58 -0300

    some changes

--- js/index.js
+++ js/index.js
@@ -1,34 +1,36 @@
 //just use the intersect observer api
-https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+//https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
 
 
 function reveal(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting)
-            return entry.target.classList.add('active');
-        return entry.target.classList.remove('active');
-    });
+  entries.forEach(entry => {
+    if (entry.isIntersecting)
+      return entry.target.classList.add('active');
+    //if I do return like the one below
+    //the animation will reset
+    //return entry.target.classList.remove('active');
+  });
 }
 
 
 //lets use the intersection api
 function setIntersectionObserver() {
-    //first define the options
-    let options = {
-        root: null, //root null = viewport
-        rootMargin: '0px',
-        threshold: 0.5
-        //how much of the element is intersecting the root, is this case how much of the element is visible
-        //, 1.0 means that the element mis 100% visible
-    }
+  //first define the options
+  let options = {
+    root: null, //root null = viewport
+    rootMargin: '0px',
+    threshold: 0.5
+    //how much of the element is intersecting the root, is this case how much of the element is visible
+    //, 1.0 means that the element mis 100% visible
+  }
 
-    let observer = new IntersectionObserver(reveal, options);
+  let observer = new IntersectionObserver(reveal, options);
 
-    var reveals = document.querySelectorAll('.reveal');
+  var reveals = document.querySelectorAll('.reveal');
 
-    for (let elem of reveals) {
-        observer.observe(elem)
-    }
+  for (let elem of reveals) {
+    observer.observe(elem)
+  }
 }
 
 setIntersectionObserver();
